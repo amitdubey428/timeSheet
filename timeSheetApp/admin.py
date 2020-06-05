5dada286eb935671bb9887faf57a6bb50a29f11a
nitasha18 <nitashag18@gmail.com>
2020-06-05 20:55:01 +0530

    foreign keys updated within tables

--- timeSheetApp/admin.py
+++ timeSheetApp/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User, Project, Timesheet, Add_Member, AdminRole
+from .models import User, Project, Timesheet , AdminRole
 
 admin.site.register(User)
 admin.site.register(Project)
-admin.site.register(Add_Member)
 admin.site.register(Timesheet)
 admin.site.register(AdminRole)
