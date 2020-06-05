5701e84faa757e265c7f50586b5dddd7d7d7a529
Amit Dubey <dubeyamit.1920@gmail.com>
2020-06-05 21:34:29 +0530

    Update admin.py

--- timeSheetApp/admin.py
+++ timeSheetApp/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-<<<<<<< HEAD
-from .models import User, Project, Timesheet, AdminRole
-=======
+
 from .models import User, Project, Timesheet , AdminRole
->>>>>>> 5dada286eb935671bb9887faf57a6bb50a29f11a
+
 
 admin.site.register(User)
 admin.site.register(Project)
